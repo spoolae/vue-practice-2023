c0f206539775261f47a69e963363208cbb2a944a
Spoolae <ohotnic147feed@gmail.com>
2023-01-13 17:12:45 +0100

    Add currency converter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,39 @@ import { createApp } from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+const cc = require("currency-codes");
+
 const app = createApp({
   data() {
     return {
+      //main
       students: [],
       search: "",
       student: { name: "", zdav: false, group: "" },
+
+      //currency
+      currency: [],
+      itemFrom: "",
+      itemTo: "",
+      amount: "",
+      result: "",
     };
   },
   mounted: function () {
     axios.get("http://34.82.81.113:3000/students").then((response) => {
       this.students = response.data;
+    });
+
+    axios.get("currency.json").then((res) => {
+      res.data.map((item) => {
+        item.nameA =
+          (cc.number(item.currencyCodeA) &&
+            cc.number(item.currencyCodeA).code) ||
+          null;
+        item.nameB =
+          cc.number(item.currencyCodeB) && cc.number(item.currencyCodeB).code;
+      });
+      this.currency = res.data;
     });
   },
   methods: {
@@ -38,6 +60,25 @@ const app = createApp({
     addStudent() {
       this.student.id = this.students.length + 1;
       this.students.push({ ...this.student });
+    },
+
+    getResult() {
+      const currentTransaction = this.currency.find(
+        (item) => item.nameA === this.itemFrom && item.nameB === this.itemTo
+      );
+      if (!currentTransaction) {
+        this.result = "Not available";
+      } else {
+        let rate;
+        if (currentTransaction.rateBuy) {
+          rate = currentTransaction.rateBuy;
+        } else if (currentTransaction.rateCross) {
+          rate = currentTransaction.rateCross;
+        } else if (currentTransaction.rateSell) {
+          rate = currentTransaction.rateSell;
+        }
+        this.result = this.amount * rate;
+      }
     },
   },
 });
