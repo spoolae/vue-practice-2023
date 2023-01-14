5bc4e665916211f86c95bf4865a7fb5adaf52a1a
Spoolae <ohotnic147feed@gmail.com>
2023-01-14 18:11:14 +0100

    Add Edit Button

--- День 2/src/components/App.vue
+++ День 2/src/components/App.vue
@@ -1,69 +1,83 @@
 <template>
-   <div id="app">
-		<input type="text" name="search" v-model="search"><br>
-		<table class="table table-dark">
-			<tr v-for="item in students"  v-bind:key="item._id">
-        <StudentsTable :item="item"></StudentsTable>
-			</tr>
-		</table>
-    <AddStudent :student="student"></AddStudent>
-			<button @click="addStudent()">Add student</button>
-	</div>
+  <div id="app">
+   <input type="text" name="search" v-model="search"><br>
+   <table class="table table-dark">
+     <tr v-for="item in students"  v-bind:key="item._id">
+       <StudentsTable :item="item" @edit="openEditForm" @delete="deleteStudent"></StudentsTable>
+     </tr>
+   </table>
+   <form v-if="isEdit" class="student-form">
+     <AddStudent :student="newStudent"></AddStudent>
+     <button @click="updateStudent(newStudent)">Save</button>
+     <button @click="cancelEdit()">Cancel</button>
+   </form>
+   <form v-else class="student-input">
+     <input v-model="student.name">
+     <input type="checkbox" v-model="student.isDonePr">
+     <select v-model="student.group">
+       <option value="RPZ 19 1/9">RPZ 19 1/9</option>
+       <option value="RPZ 19 2/9">RPZ 19 2/9</option>
+     </select>
+   <button @click="addStudent()">Add student</button>
+   </form>
+ </div>
 </template>
 <script>
 import axios from 'axios'
 import AddStudent from './AddStudent.vue';
 import StudentsTable from './StudentsTable.vue';
 export default {
-  components: {
-    AddStudent,
-    StudentsTable
-  },
-  data() {
-    return {
-      students: [],
-      search: "",
-      student: { name: "", isDonePr: false, group: "" },
-      isEdit:false,
-    };
-  },
-  mounted() {
-    axios.get("http://34.82.81.113:3000/students").then((res)=>{
-      this.students = res.data;
-    })
-  },
-  methods: {
+ components: {
+   AddStudent,
+   StudentsTable
+ },
+ data() {
+   return {
+     students: [],
+     search: "",
+     student: { name: "", isDonePr: false, group: "" },
+     isEdit:false,
+     newStudent: {}
+   };
+ },
+ mounted() {
+   axios.get("http://34.82.81.113:3000/students").then((res)=>{
+     this.students = res.data;
+   })
+ },
+ methods: {addStudent() {
+  axios.post("http://34.82.81.113:3000/students",{ ...this.student }).then(
+    (response)=>{
+      this.students.push(response.data);
+    }
+  );
 
-    addStudent() {
-      axios.post("http://34.82.81.113:3000/students",{ ...this.student }).then(
-        (response)=>{
-          this.students.push(response.data);
-        }
-      );
+},
+deleteStudent(studId) {
+  axios.delete(`http://34.82.81.113:3000/students/${studId}`).then(()=>{
+    this.students = this.students.filter((item)=>item._id !== studId);
+  });
+},
+updateStudent(newStudent) {
+  axios.put(`http://34.82.81.113:3000/students/${newStudent._id}`,
+  {
+    name:newStudent.name,
+    isDonePr:newStudent.isDonePr,
+    group:newStudent.group
+  }).then(
+    (response)=>{
+      console.log(response);
+      this.isEdit = false;
+    }
+  );
 
-    },
-    deleteStudent(studId) {
-      axios.delete(`http://34.82.81.113:3000/students/${studId}`).then(()=>{
-        this.students = this.students.filter((item)=>item._id !== studId);
-      });
-    },
-    updateStudent(newStudent) {
-      axios.put(`http://34.82.81.113:3000/students/${newStudent._id}`,
-      {
-        name:newStudent.name,
-        isDonePr:newStudent.isDonePr,
-        group:newStudent.group
-      }).then(
-        (response)=>{
-          console.log(response);
-        }
-      );
-
-    },
-  },
-  }
- </script>
-
-<style scoped>
-
-</style>
+},
+openEditForm(item) {
+  this.newStudent = item;
+  this.isEdit = true;
+},
+cancelEdit() {
+  this.isEdit = false;
+}},
+}
+</script>
--- День 2/src/components/StudentsTable.vue
+++ День 2/src/components/StudentsTable.vue
@@ -3,45 +3,18 @@
     <td>{{item.name}}</td>
     <td><input type="checkbox" v-model="item.isDonePr"></td>
     <td>{{item.group}}</td>
-    <td><a href = "#" @click="deleteStudent(item._id)">Видалити</a></td>
+    <td><a href = "#" @click="$emit('delete', item._id)">Delete</a></td>
+    <td><a href="#" @click="$emit('edit', item)">Edit</a></td>
   </template>
-  <AddStudent :student="newStudent" v-else></AddStudent>
-  <td v-if="!isEdit"><a href = "#" @click="isEdit = !isEdit,newStudent = item">Редагувати</a></td>
-  <td v-else><a href = "#" @click="updateStudent(newStudent)">Редагувати</a></td>
-
 </template>
-
-<script >
-import { defineComponent } from 'vue';
-import AddStudent from './AddStudent.vue';
-export default defineComponent({
+<script>
+export default {
   name: "StudentsTable",
-   components: {
-    AddStudent
-  },
   props: {
     item: {
       type: Object,
       require: true
     }
-  },
-   data() {
-    return {
-      isEdit:false,
-      newStudent:{},
-    };
-  },
-   methods:{
-    deleteStudent(studId) {
-      this.$parent.deleteStudent(studId);
-    },
-    updateStudent(newStudent) {
-      this.isEdit=!this.isEdit;
-      this.$parent.updateStudent(newStudent);
-    }
   }
-
-});
+};
 </script>
-
-
