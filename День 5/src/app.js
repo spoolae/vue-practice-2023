06bd226c252bc2384bfdd15e6211695944cbc266
Spoolae <ohotnic147feed@gmail.com>
2023-01-20 11:37:41 +0100

    День 5

---
+++ День 5/src/app.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import { router } from "./Router/index";
+import App from "./App.vue";
+import reset from "reset-css";
+
+createApp(App).use(router).use(reset).mount("#app");
