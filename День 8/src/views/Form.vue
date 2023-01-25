7f092dab2196817e20f08ea2b26c0053f7d408b5
Spoolae <ohotnic147feed@gmail.com>
2023-01-25 13:14:02 +0100

    Refactoring

--- День 8/src/views/Form.vue
+++ День 8/src/views/Form.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full max-w-screen-md ml-auto mr-auto mt-2 text-center">
-    <h2 class="text-gray-500 text-3xl font-bold mb-4">
+    <h2 class="text-gray-500 text-3xl font-bold mb-4 mt-6">
       Повідомити про корупцію в навчальному закладі
     </h2>
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div v-for="item in dataInputList">
-        <label>{{ item.msg }}</label>
+      <div v-for="item in requiredFields" class="text-left">
+        <label style="line-height: 2">{{ item.msg }}</label>
         <input
           v-model="item.inputData"
           :name="item.variable"
@@ -25,8 +25,12 @@
           Вказати особисту інформацію
         </label>
       </div>
-      <div v-if="showOptionalFields" v-for="item in dataInputListOptional">
-        <label>{{ item.msg }}</label>
+      <div
+        v-if="showOptionalFields"
+        v-for="item in optionalFields"
+        class="text-left"
+      >
+        <label style="line-height: 2">{{ item.msg }}</label>
         <input
           v-model="item.inputData"
           :name="item.variable"
@@ -43,19 +47,20 @@
     </form>
     <button
       @click="sendEmail"
-      class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+      class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full"
       type="button"
     >
       Відправити
     </button>
   </div>
 </template>
+
 <script>
 export default {
   name: "Form",
   data() {
     return {
-      dataInputList: [
+      requiredFields: [
         {
           msg: "ПІБ особи яка ймовірно вчинила правопорушення",
           inputData: "",
@@ -70,7 +75,7 @@ export default {
           variable: "description",
         },
       ],
-      dataInputListOptional: [
+      optionalFields: [
         { msg: "Ваше ПІБ", inputData: "", variable: "fromName" },
         { msg: "Номер телефону", inputData: "", variable: "fromPhone" },
       ],
@@ -80,10 +85,10 @@ export default {
   },
   methods: {
     sendEmail(e) {
-      this.dataInputList.forEach((item) => {
+      this.requiredFields.forEach((item) => {
         this.formData[item.variable] = item.inputData;
       });
-      this.dataInputListOptional.forEach((item) => {
+      this.optionalFields.forEach((item) => {
         if (!item.inputData) {
           this.formData[item.variable] = "Анонімно";
         } else {
